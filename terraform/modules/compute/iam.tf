9c754f537caacd3d883aa0ec178e688fd9d759cd
Adesokan segun <sheezycouture@gmail.com>
2026-02-08 17:42:45 +0100

    changes in infra

--- terraform/modules/compute/iam.tf
+++ terraform/modules/compute/iam.tf
@@ -1,4 +1,6 @@
-# IAM for EC2 (SSM + Secrets)
+############################################
+# EC2 ROLE
+############################################
 
 resource "aws_iam_role" "ec2" {
   name = "${var.project_name}-${var.environment}-ec2-role"
@@ -13,16 +15,18 @@ resource "aws_iam_role" "ec2" {
   })
 }
 
-
-# Attach SSM policy to EC2 role
+############################################
+# SSM ACCESS
+############################################
 
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-
-# Allow read access to Secrets Manager
+############################################
+# SECRETS MANAGER READ
+############################################
 
 resource "aws_iam_policy" "secrets_read" {
   name = "${var.project_name}-${var.environment}-secrets-read"
@@ -42,45 +46,78 @@ resource "aws_iam_role_policy_attachment" "secrets" {
   policy_arn = aws_iam_policy.secrets_read.arn
 }
 
+############################################
+# EC2 DOWNLOAD ARTIFACTS
+############################################
 
-# Instance Profile for EC2
+resource "aws_iam_policy" "artifact_read" {
+  name = "${var.project_name}-${var.environment}-artifact-read"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = ["s3:GetObject"]
+      Resource = "arn:aws:s3:::votingapp-artifacts-dev-12345/*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "artifact_read" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = aws_iam_policy.artifact_read.arn
+}
+
+############################################
+# INSTANCE PROFILE
+############################################
 
 resource "aws_iam_instance_profile" "this" {
   name = "${var.project_name}-${var.environment}-instance-profile"
   role = aws_iam_role.ec2.name
 }
 
-# s3 bucket access policy for Ansible deployment
-resource "aws_iam_policy" "ansible_ssm_s3" {
-  name = "${var.project_name}-${var.environment}-ansible-ssm-s3"
+############################################
+# GITHUB USER
+############################################
+
+resource "aws_iam_user" "github" {
+  name = "${var.project_name}-${var.environment}-github"
+}
+
+resource "aws_iam_policy" "github_artifact_upload" {
+  name = "${var.project_name}-${var.environment}-github-upload"
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:DeleteObject"
-        ]
-        Resource = "arn:aws:s3:::votingapp-tf-state-dev-123456/ansible-ssm/*"
-      },
-      {
-        Effect = "Allow"
-        Action = ["s3:ListBucket"]
-        Resource = "arn:aws:s3:::votingapp-tf-state-dev-123456"
-        Condition = {
-          StringLike = {
-            "s3:prefix": ["ansible-ssm/*"]
-          }
-        }
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:PutObject"
+      ],
+      Resource = "arn:aws:s3:::votingapp-artifacts-dev-12345/*"
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ansible_ssm_s3" {
-  role       = aws_iam_role.ec2.name
-  policy_arn = aws_iam_policy.ansible_ssm_s3.arn
+resource "aws_iam_user_policy_attachment" "github_attach" {
+  user       = aws_iam_user.github.name
+  policy_arn = aws_iam_policy.github_artifact_upload.arn
+}
+
+resource "aws_iam_access_key" "github" {
+  user = aws_iam_user.github.name
+}
+
+############################################
+# OUTPUTS 
+############################################
+
+output "github_access_key_id" {
+  value = aws_iam_access_key.github.id
+}
+
+output "github_secret_access_key" {
+  value     = aws_iam_access_key.github.secret
+  sensitive = true
 }
