1c6e25dd76f063a7754a4a6e7ea214d6235600fd
Adesokan segun <sheezycouture@gmail.com>
2026-02-08 18:54:07 +0100

    disable db during tests

--- src/test/java/com/MySpringBootAa23vVotingAppFinalApplicationTests.java
+++ src/test/java/com/MySpringBootAa23vVotingAppFinalApplicationTests.java
@@ -3,11 +3,17 @@ package com;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+  properties = {
+    "spring.autoconfigure.exclude=" +
+    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+  }
+)
 class MySpringBootAa23vVotingAppFinalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
