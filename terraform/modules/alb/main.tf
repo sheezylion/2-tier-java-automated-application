d7b323704636b12c2f32f9527d940bc381f112fd
Adesokan segun <sheezycouture@gmail.com>
2026-02-15 20:52:39 +0100

    changes in ansible

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -2,6 +2,7 @@
 
 resource "aws_lb" "this" {
   name               = "${var.project_name}-${var.environment}-alb"
+  internal           = false
   load_balancer_type = "application"
   subnets            = var.public_subnet_ids
   security_groups    = [var.alb_sg_id]
